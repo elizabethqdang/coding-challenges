1af5be961c7c09780aa42decd21ec816d3399959
Elizabeth Dang <elizabethqdang@gmail.com>
2022-01-20 15:00:31 -0800

    index.js

--- qawolf-take-home/index.js
+++ qawolf-take-home/index.js
@@ -1,5 +1,5 @@
 const { chromium } = require("playwright");
-const { test } = require("@playwright/test");
+// const { test } = require("@playwright/test");
 
 (async () => {
   const browser = await chromium.launch({
@@ -13,6 +13,23 @@ const { test } = require("@playwright/test");
   await page.fill('input[name="userLoginId"]', 'USERNAME');
   await page.fill('input[name="password"]', 'PASSWORD');
   await page.click('text=Sign In');
+
+  await page.screenshot({ path: 'screenshot.png', fullPage: true});
+  
+  page.on('console.', msg => console.log(msg.text()));
+  page.on('console', msg => {
+    if (msg.type() === 'error')
+      console.log(`Error text: "${msg.text()}"`);
+  });
+
+  // const [msg] = await Promise.all([
+  //   page.waitForEvent('console')
+  // ]);
+
+  page.on('pageerror', exception => {
+    console.log(`Uncaught exception: "${exception}"`);
+  });
+  
 
   await browser.close();
 })();
